f15f2981f0b584ce31908efbe83544aa238932b6
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-20 06:46:49 +0100

    new

--- src/main/java/org/bram/data/repositories/CustomerRepository.java
+++ src/main/java/org/bram/data/repositories/CustomerRepository.java
@@ -1,4 +1,6 @@
 package org.bram.data.repositories;
 
-public class CustomerRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public class CustomerRepository extends MongoRepository<Customer, String> {
 }
