058fc0712aff31ea80aebad65eb2e83d5a2957c9
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-20 06:11:37 +0100

    new

--- src/main/java/org/bram/data/models/User.java
+++ src/main/java/org/bram/data/models/User.java
@@ -3,5 +3,9 @@ package org.bram.data.models;
 public class User {
 
     private String firstName;
+    private String lastName;
+    private String email;
+    pricat
+    private String password;
 
 }
