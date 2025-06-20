3a5089640a67ef5bf2e39048b4c6db589e6e9765
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-20 07:22:37 +0100

    new

--- src/main/java/org/bram/Main.java
+++ src/main/java/org/bram/Main.java
@@ -1,8 +1,7 @@
 package org.bram;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.swing.*;
 
 @SpringBootApplication
 public class Main {
