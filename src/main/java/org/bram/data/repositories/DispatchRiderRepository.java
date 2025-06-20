5435f1a348ba342ae865af2c19a1762ce99b3f89
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-20 07:05:41 +0100

    first commit

--- src/main/java/org/bram/data/repositories/DispatchRiderRepository.java
+++ src/main/java/org/bram/data/repositories/DispatchRiderRepository.java
@@ -2,6 +2,8 @@ package org.bram.data.repositories;
 
 import org.bram.data.models.DispatchRider;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DispatchRiderRepository extends MongoRepository<DispatchRider, String> {
 }
