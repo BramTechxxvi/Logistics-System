753b15f1e5f2bbd810d9ad7f437d4b3b7c74dc61
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-20 07:06:21 +0100

    first commit

--- src/main/java/org/bram/data/repositories/DeliveryTaskRepository.java
+++ src/main/java/org/bram/data/repositories/DeliveryTaskRepository.java
@@ -2,6 +2,8 @@ package org.bram.data.repositories;
 
 import org.bram.data.models.DeliveryTask;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeliveryTaskRepository extends MongoRepository<DeliveryTask, String> {
 }
