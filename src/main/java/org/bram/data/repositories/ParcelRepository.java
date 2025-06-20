482f50d306ea19f4cc2a51a89b3b82ef2242e27f
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-20 07:04:39 +0100

    first commit

---
+++ src/main/java/org/bram/data/repositories/ParcelRepository.java
@@ -0,0 +1,6 @@
+package org.bram.data.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ParcelRepository extends MongoRepository<ParcelRepository, String> {
+}
