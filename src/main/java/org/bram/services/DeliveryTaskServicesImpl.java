34e0d080dca4b636798b67fe8b3746c6116b603f
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-20 09:08:11 +0100

    first commit

--- src/main/java/org/bram/services/DeliveryTaskServicesImpl.java
+++ src/main/java/org/bram/services/DeliveryTaskServicesImpl.java
@@ -3,5 +3,5 @@ package org.bram.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DeliveryTaskServicesImpl {
+public class DeliveryTaskServicesImpl implements DeliveryTaskServices{
 }
