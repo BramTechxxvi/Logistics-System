ba1d3b013c750a3999dd77deece2d87db94fe218
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-20 07:21:29 +0100

    first commit

--- src/main/java/org/bram/Main.java
+++ src/main/java/org/bram/Main.java
@@ -1,11 +1,12 @@
 package org.bram;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import javax.swing.*;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
         SpringApplication.run(Main.class, args);
     }
 }
