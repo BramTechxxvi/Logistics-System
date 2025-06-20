9184e50c6f68cfdb2f49db0f87afb3ec21ac870c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-20 07:01:21 +0100

    first commit

--- src/main/java/org/bram/data/models/DeliveryTask.java
+++ src/main/java/org/bram/data/models/DeliveryTask.java
@@ -1,4 +1,11 @@
 package org.bram.data.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
 public class DeliveryTask {
+
+    @Id
+    private String id;
 }
--- src/main/java/org/bram/data/repositories/DeliveryTaskRepository.java
+++ src/main/java/org/bram/data/repositories/DeliveryTaskRepository.java
@@ -1,4 +1,7 @@
 package org.bram.data.repositories;
 
-public interface DeliveryTaskRepository {
+import org.bram.data.models.DeliveryTask;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface DeliveryTaskRepository extends MongoRepository<DeliveryTask, String> {
 }
