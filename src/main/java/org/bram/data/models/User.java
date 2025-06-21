b71c45dc147b9af0e92fe9138ae06050e6902e16
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-21 21:37:00 +0100

    new

--- src/main/java/org/bram/data/models/User.java
+++ src/main/java/org/bram/data/models/User.java
@@ -1,10 +1,12 @@
 package org.bram.data.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 public class User {
 
+    @Id
     private String id;
     private String firstName;
     private String lastName;
