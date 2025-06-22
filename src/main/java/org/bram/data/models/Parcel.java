cd679ec21cc1afdddc071764e1d133bdea705fdc
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-22 14:35:12 +0100

    first commit

--- src/main/java/org/bram/data/models/Parcel.java
+++ src/main/java/org/bram/data/models/Parcel.java
@@ -1,9 +1,13 @@
 package org.bram.data.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection="Parcel")
 public class Parcel {
+
+    @Id
+    private String id;
 }
