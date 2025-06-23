b454ac8feab29ca06bc189c664d0df6ca361ca40
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-24 00:15:28 +0100

    first commit

--- src/main/java/org/bram/services/CustomerServicesImpl.java
+++ src/main/java/org/bram/services/CustomerServicesImpl.java
@@ -1,7 +1,30 @@
 package org.bram.services;
 
+import org.bram.data.repositories.CustomerRepository;
+import org.bram.dtos.request.CustomerRegisterRequest;
+import org.bram.dtos.request.LoginRequest;
+import org.bram.dtos.response.CustomerRegisterResponse;
+import org.bram.dtos.response.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServicesImpl implements CustomerServices{
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServicesImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public CustomerRegisterResponse registerCustomer(CustomerRegisterRequest request) {
+        return null;
+    }
+
+    @Override
+    public LoginResponse login(LoginRequest request) {
+        return null;
+    }
 }
