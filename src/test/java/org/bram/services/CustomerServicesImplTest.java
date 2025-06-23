02fec2f1dff87705ea65a17bc290d4535fe0d89d
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-24 00:31:41 +0100

    first commit

--- src/test/java/org/bram/services/CustomerServicesImplTest.java
+++ src/test/java/org/bram/services/CustomerServicesImplTest.java
@@ -1,7 +1,20 @@
 package org.bram.services;
 
+import org.bram.data.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CustomerServicesImplTest {
+
+    private CustomerServices customerServices;
+    private CustomerRepository customerRepository;
+
+    @BeforeEach
+    void setUp() {
+        customerRepository.deleteAll();
+    }
 
 }
