e68f60967944b1acfcef1ad545c113aa0adde535
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-24 00:35:11 +0100

    first commit

--- src/test/java/org/bram/services/CustomerServicesImplTest.java
+++ src/test/java/org/bram/services/CustomerServicesImplTest.java
@@ -1,7 +1,11 @@
 package org.bram.services;
 
 import org.bram.data.repositories.CustomerRepository;
+import org.bram.dtos.request.CustomerRegisterRequest;
+import org.bram.dtos.response.CustomerRegisterResponse;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,12 +13,25 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class CustomerServicesImplTest {
 
+    @Autowired
     private CustomerServices customerServices;
+    @Autowired
     private CustomerRepository customerRepository;
+    private CustomerRegisterRequest registerRequest;
+    private CustomerRegisterResponse registerResponse;
 
     @BeforeEach
     void setUp() {
         customerRepository.deleteAll();
+        registerRequest = new CustomerRegisterRequest();
+        registerResponse = new CustomerRegisterResponse();
+    }
+
+    @Test
+    public void krmgleg() {}
+
+    private void registerCustomer() {
+
     }
 
 }
