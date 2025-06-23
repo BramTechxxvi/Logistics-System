0db2003223c907bcdae28f4d134566e3f7986c5a
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-23 09:26:36 +0100

    first commit

--- src/main/java/org/bram/services/CustomerServices.java
+++ src/main/java/org/bram/services/CustomerServices.java
@@ -1,4 +1,13 @@
 package org.bram.services;
 
+import org.bram.dtos.request.*;
+import org.bram.dtos.response.*;
+
 public interface CustomerServices {
+
+    CustomerRegisterResponse registerCustomer(CustomerRegisterRequest request);
+    LoginResponse login(LoginRequest request);
+
+
+
 }
